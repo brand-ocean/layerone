3d74b9af8ac9f75906d9fb1cc22fa365b7dc4ac5
brandocean <info@brandocean.nl>
2023-09-18 16:24:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 // Store DOM elements in variables for better performance and readability
 const eyeForTalentElem = document.querySelector("#eyefortalent");
 
+// Split and animate text on document load
+document.addEventListener("DOMContentLoaded", () => {
+    // Split the text inside #eyefortalent into individual words
+    let words = eyeForTalentElem.innerText.split(' ');
+    eyeForTalentElem.innerHTML = words.map(word => `<span class="word">${word}</span> `).join('');
+
+    // Hide all words initially
+    gsap.set(".word", { visibility: "hidden" });
+
+    // Make each word visible one-by-one
+    gsap.to(".word", {
+        visibility: "visible",
+        stagger: 0.5,
+    });
+});
+
 // Main gsap animation for the #eyefortalent element
 gsap.to(eyeForTalentElem, {
     scrollTrigger: {
         trigger: "container-hero",
         start: "top",
         end: "bottom",
-        scrub: 1,
+        scrub: 0.5,
         pin: true,
         anticipatePin: 1,
-        markers: true,
+        markers: false,
     },
-    scale: 20,
+    scale: 10,
     ease: "power2.out",
     filter: "blur(5px)",
-    opacity: 0,
-    onComplete: () => gsap.set(eyeForTalentElem, { color: "#f5f6fa" })  // Arrow function for brevity
-});
-
-// Function to split and animate text when the document is loaded
-document.addEventListener("DOMContentLoaded", () => {
-
-    // Split the text inside #eyefortalent into individual words
-    let words = eyeForTalentElem.innerText.split(' ');
-    eyeForTalentElem.innerHTML = words.map(word => `<span class="word">${word}</span> `).join('');
-
-    // Apply the intro animation to each word
-    gsap.from(".word", {
-        duration: 2,
-        opacity: 0,
-        stagger: 0.5,
-        ease: "elastic.out(1, 0.3)"
-    });
-
+    onComplete: () => gsap.set(eyeForTalentElem, { color: "#f5f6fa" })
 });
 
 // Additional gsap animation for changing the color
@@ -41,8 +39,8 @@ gsap.to(eyeForTalentElem, {
     color: "#a69285",
     scrollTrigger: {
         trigger: eyeForTalentElem,
-        start: "top center",
-        end: "bottom center",
-        scrub: 1,
+        start: "top",
+        end: "bottom",
+        scrub: 0.5,
     }
 });
