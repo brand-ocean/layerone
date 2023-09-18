374584ee698e4c933fe20220519fb88202fa1a1e
Arin Issa <129693517+brand-ocean@users.noreply.github.com>
2023-09-18 16:04:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// Store DOM elements in variables for better performance and readability
-const eyeForTalentElem = document.querySelector("#eyefortalent");
-const navbarHeight = 5 * parseFloat(getComputedStyle(document.documentElement).fontSize); // Calculate 5rem in pixels
+// Constants
+const TEXT_SPLIT_CLASS = "word";
 
-// Main gsap animation for the #eyefortalent element
+// GSAP Animations
+
+// Main animation for the #eyefortalent element
 gsap.to(eyeForTalentElem, {
     scrollTrigger: {
-        trigger: "#container-hero", // Make sure to use a proper selector here
-start: "top",
-        end: "bottom center", // Keep the end position as it is
+        trigger: "container-hero",
+        start: "top",
+        end: "bottom",
         scrub: 1,
         pin: true,
         anticipatePin: 1,
@@ -17,34 +18,20 @@ start: "top",
     ease: "power2.out",
     filter: "blur(5px)",
     opacity: 0,
-    onComplete: () => gsap.set(eyeForTalentElem, { color: "#f5f6fa" }) // Arrow function for brevity
+    onComplete: () => gsap.set(eyeForTalentElem, { color: "#f5f6fa" })
 });
 
-// Function to split and animate text when the document is loaded
-document.addEventListener("DOMContentLoaded", () => {
-
-    // Split the text inside #eyefortalent into individual words
-    let words = eyeForTalentElem.innerText.split(' ');
-    eyeForTalentElem.innerHTML = words.map(word => `<span class="word">${word}</span> `).join('');
-
-    // Apply the intro animation to each word
-    gsap.from(".word", {
-        duration: 2,
-        opacity: 0,
-        start: "top+=10rem",
-        stagger: 0.5,
-        ease: "elastic.out(1, 0.3)"
-    });
-
-});
-
-// Additional gsap animation for changing the color
+// Animation for changing the color of #eyefortalent element
 gsap.to(eyeForTalentElem, {
     color: "#a69285",
     scrollTrigger: {
         trigger: eyeForTalentElem,
-        start: `top+=${navbarHeight}px`, // Adjusted the start position to include the navbar height
-        end: "bottom center", // Keep the end position as it is
+        start: "top",
+        end: "bottom center",
         scrub: 1,
     }
 });
+
+// Event Listeners
+
+// Function to split and animate text when the
