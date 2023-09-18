71bce70178514cbc52dfbe2d601c926e93bdf037
Arin Issa <129693517+brand-ocean@users.noreply.github.com>
2023-09-18 16:23:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-// Constants
-const TEXT_SPLIT_CLASS = "word";
+// Store DOM elements in variables for better performance and readability
+const eyeForTalentElem = document.querySelector("#eyefortalent");
 
-// GSAP Animations
+// Split and animate text on document load
+document.addEventListener("DOMContentLoaded", () => {
+    // Split the text inside #eyefortalent into individual words
+    let words = eyeForTalentElem.innerText.split(' ');
+    eyeForTalentElem.innerHTML = words.map(word => `<span class="word">${word}</span> `).join('');
 
-// Main animation for the #eyefortalent element
+    // Hide all words initially
+    gsap.set(".word", { visibility: "hidden" });
+
+    // Make each word visible one-by-one
+    gsap.to(".word", {
+        visibility: "visible",
+        stagger: 0.5,
+    });
+});
+
+// Main gsap animation for the #eyefortalent element
 gsap.to(eyeForTalentElem, {
     scrollTrigger: {
         trigger: "container-hero",
         start: "top",
         end: "bottom",
-        scrub: 1,
+        scrub: 0.5,
         pin: true,
         anticipatePin: 1,
         markers: false,
     },
-    scale: 20,
+    scale: 10,
     ease: "power2.out",
     filter: "blur(5px)",
-    opacity: 0,
     onComplete: () => gsap.set(eyeForTalentElem, { color: "#f5f6fa" })
 });
 
-// Animation for changing the color of #eyefortalent element
+// Additional gsap animation for changing the color
 gsap.to(eyeForTalentElem, {
     color: "#a69285",
     scrollTrigger: {
         trigger: eyeForTalentElem,
         start: "top",
-        end: "bottom center",
-        scrub: 1,
+        end: "bottom",
+        scrub: 0.5,
     }
 });
-
-// Event Listeners
-
-// Function to split and animate text when the
