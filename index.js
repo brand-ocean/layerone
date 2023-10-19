a93fa7d9bab0f4a367fbdc39e026954e4472201b
brandocean <info@brandocean.nl>
2023-10-19 14:58:08 +0200

    v1

--- index.js
+++ index.js
@@ -1,46 +1,76 @@
-// Store DOM elements in variables for better performance and readability
-const eyeForTalentElem = document.querySelector("#eyefortalent");
-
-// Split and animate text on document load
-document.addEventListener("DOMContentLoaded", () => {
-    // Split the text inside #eyefortalent into individual words
-    let words = eyeForTalentElem.innerText.split(' ');
-    eyeForTalentElem.innerHTML = words.map(word => `<span class="word">${word}</span> `).join('');
-
-    // Hide all words initially
-    gsap.set(".word", { visibility: "hidden" });
-
-    // Make each word visible one-by-one
-    gsap.to(".word", {
-        visibility: "visible",
-        stagger: 0.5,
-    });
-});
-
-// Main gsap animation for the #eyefortalent element
-gsap.to(eyeForTalentElem, {
-    scrollTrigger: {
-        trigger: "container-hero",
-        start: "top",
-        end: "bottom",
-        scrub: 0.5,
-        pin: true,
-        anticipatePin: 1,
-        markers: false,
-    },
-    scale: 10,
-    ease: "power2.out",
-    filter: "blur(5px)",
-    onComplete: () => gsap.set(eyeForTalentElem, { color: "#f5f6fa" })
-});
-
-// Additional gsap animation for changing the color
-gsap.to(eyeForTalentElem, {
-    color: "#a69285",
-    scrollTrigger: {
-        trigger: eyeForTalentElem,
-        start: "top",
-        end: "bottom",
-        scrub: 0.5,
+$(".nav_wrap").each(function () {
+    let hamburgerEl = $(this).find(".nav_hamburger_wrap");
+    let navLineEl = $(this).find(".nav_hamburger_line");
+    let menuContainEl = $(this).find(".menu_contain");
+    let flipItemEl = $(this).find(".nav_hamburger_base");
+    let menuWrapEl = $(this).find(".menu_wrap");
+    let menuBaseEl = $(this).find(".menu_base");
+    let menuLinkEl = $(this).find(".menu_link");
+  
+    let flipDuration = 0.6;
+  
+    function flip(forwards) {
+      let state = Flip.getState(flipItemEl);
+      if (forwards) {
+        flipItemEl.appendTo(menuContainEl);
+      } else {
+        flipItemEl.appendTo(hamburgerEl);
+      }
+      Flip.from(state, { duration: flipDuration });
     }
-});
+  
+    let tl = gsap.timeline({ paused: true });
+    tl.set(menuWrapEl, { display: "flex" });
+    tl.from(menuBaseEl, {
+      opacity: 0,
+      duration: flipDuration,
+      ease: "none",
+      onStart: () => {
+        flip(true);
+      }
+    });
+    tl.to(navLineEl.eq(0), { y: 4, rotate: 45, duration: flipDuration }, "<");
+    tl.to(navLineEl.eq(1), { y: -4, rotate: -45, duration: flipDuration }, "<");
+    tl.from(menuLinkEl, {
+      opacity: 0,
+      yPercent: 50,
+      duration: 0.2,
+      stagger: { amount: 0.2 },
+      onReverseComplete: () => {
+        flip(false);
+      }
+    });
+  
+    function openMenu(open) {
+      if (!tl.isActive()) {
+        if (open) {
+          tl.play();
+          hamburgerEl.addClass("nav-open");
+        } else {
+          tl.reverse();
+          hamburgerEl.removeClass("nav-open");
+        }
+      }
+    }
+  
+    hamburgerEl.on("click", function () {
+      if ($(this).hasClass("nav-open")) {
+        openMenu(false);
+      } else {
+        openMenu(true);
+      }
+    });
+  
+    menuBaseEl.on("mouseenter", function () {
+      openMenu(false);
+    });
+    menuBaseEl.on("click", function () {
+      openMenu(false);
+    });
+  
+    $(document).on("keydown", function (e) {
+      if (e.key === "Escape") {
+        openMenu(false);
+      }
+    });
+  });
